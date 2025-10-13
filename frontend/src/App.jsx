bcb31b91f083994202bae3bba590da960d252587
Ali Issa <alyy.issa01@gmail.com>
2025-10-13 18:11:43 +0300

    Added Course Page and Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,11 @@ import ForgetPass from './pages/Auth/ForgetPass'
 import NotFound from './pages/Error.jsx/NotFound'
 import ContactUs from './pages/ContactUs'
 import Course from './pages/Course'
+import Exam from './pages/Exam'
+import CourseDescription from './components/Course/CourseDescription'
+import CourseChapters from './components/Course/CourseChapters'
+import CourseComments from './components/Course/CourseComments'
+import CourseExam from './components/Course/CourseExam'
 
 function App() {
   return (
@@ -19,7 +24,14 @@ function App() {
         <Route path='/forgetpass' element={<ForgetPass />}/>
         <Route path='/error' element={<NotFound />}/>
         <Route path='/contactus' element={<ContactUs />}/>
-        <Route path='/course' element={<Course />}/>
+        {/* Nested For Course */}
+          <Route path='/courses/:courseId' element={<Course />}>
+            <Route path='' element={<CourseDescription />} />
+            <Route path="chapters" element={<CourseChapters />} />
+            <Route path="comments" element={<CourseComments />} />
+            <Route path='exam' element={<CourseExam />}/>
+          </Route>
+        <Route path='/exam' element={<Exam />}/>
       </Routes>
     </div>
   )
